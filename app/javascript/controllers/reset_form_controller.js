ed0cd4afb6a0406abf325d77279c2d4803214a41
Robert Ferney <rob@ferney.org>
2022-01-04 09:21:38 -0700

    form reset working

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,7 +1,8 @@
-import { Controller } from "stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   reset() {
+    console.log('reset()',this);
     this.element.reset();
   }
 }
